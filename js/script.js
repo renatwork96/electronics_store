288538afaaa40d1bea20a70a82b9de7d8ce4b5df
renatwork96 <renat.work96@gmail.com>
2021-05-14 12:39:38 +0300

    added slider top & fix modal window

--- js/script.js
+++ js/script.js
@@ -1,21 +1,87 @@
 'use strict';
-const toggleMenu = () => {
 
-  const btnMenu = document.querySelectorAll('.callback-btn'),
+//Меню
+const modalWindow = () => {
+
+  const callbackBtn = document.querySelectorAll('.callback-btn'),
     modalCallback = document.querySelector('.modal-callback'),
     modalOverlay = document.querySelector('.modal-overlay'),
-    modalClose = document.querySelector('.modal-close');
-
-  const handlerMenu = () => {
+    modalClose = document.querySelector('.modal-close'),
+    imgBtnSlider = document.querySelectorAll('.modal-btn-slider'),
+    buttonServices = document.querySelector('.button-services');
+    
+  
+  const handlerModal = () => {
     modalCallback.classList.toggle('modal-callback--visible');
     modalOverlay.classList.toggle('modal-overlay--visible');
   };
 
-  btnMenu[0].addEventListener('click', handlerMenu);
-  btnMenu[1].addEventListener('click', handlerMenu);
-  modalClose.addEventListener('click', handlerMenu);
-  modalOverlay.addEventListener('click', handlerMenu);
+  const addEventsItem = (selector, funct) => {
+    selector.forEach(function(item){
+      item.addEventListener('click', funct);
+    });
+  };
+
+  addEventsItem(callbackBtn, handlerModal);
+  addEventsItem(imgBtnSlider, handlerModal);
+  buttonServices.addEventListener('click', handlerModal);
+  modalClose.addEventListener('click', handlerModal);
+  modalOverlay.addEventListener('click', handlerModal);
 
 };
 
-toggleMenu();
+modalWindow();
+
+
+//Слайдер Top
+const sliderTop = () => {
+  const slide = document.querySelectorAll('.top-slider>.item');
+
+  let currentSlide = 0;
+
+  const prevSlide = (elem, index, strClassAct, strClassPrev) => {
+    elem[index].classList.remove(strClassAct);
+    elem[index].classList.add(strClassPrev);
+  };
+
+  const nextSlide = (elem, index, strClassAct, strClassPrev) => {
+    elem[index].classList.add(strClassAct);
+    elem[index].classList.remove(strClassPrev);
+  };
+
+  const autoPlaySlide = () => {
+    
+    prevSlide(slide, currentSlide, 'item--active', 'item--noactive');
+    currentSlide++;
+
+    if (currentSlide >= slide.length) {
+      currentSlide = 0;
+    }
+
+    nextSlide(slide, currentSlide, 'item--active', 'item--noactive');
+  };
+
+  const startSlide = (time = 3000) => {
+    setInterval(autoPlaySlide, time);
+  };
+
+  startSlide(3000);
+};
+
+
+sliderTop();
+
+
+//Слайдер Top
+const sliderServices = () => {
+  const sliderServicesItem = document.querySelectorAll('.slider-services-item'),
+    arrowRight = document.querySelector('.arrow-right'),
+    arrowLeft = document.querySelector('.arrow-left');
+
+  let currentSlide = 0;
+
+  
+
+};
+
+sliderServices();
