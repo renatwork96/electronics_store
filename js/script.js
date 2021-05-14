4866425dd52456956ae95bff61cc9872fb0bc997
renatwork96 <renat.work96@gmail.com>
2021-05-14 13:56:05 +0300

    added accordeon

--- js/script.js
+++ js/script.js
@@ -103,3 +103,40 @@ const sliderServices = () => {
 };
 
 sliderServices();
+
+//Аккордеон
+const accordeon = () => {
+  const accordeon = document.querySelector('.accordeon');
+  const accordeonItems = document.querySelectorAll('.accordeon>.element');
+
+  const openItem = (item) => {
+    item.parentNode.classList.add('active');
+  };
+
+  const closeItems = () => {
+    accordeonItems.forEach(function(item){
+      item.classList.remove('active');
+    });
+  };
+
+  accordeon.addEventListener('click', (event) => {
+
+    event.preventDefault();
+    let target = event.target;
+
+    if(target.matches('.title')) {
+      
+      if(target.parentNode.classList.contains('active')) {
+        closeItems();
+      } else {
+        closeItems();
+        openItem(target);
+      }
+      
+    }
+
+  });
+
+};
+
+accordeon();
